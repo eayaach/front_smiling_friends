93a1a8fe3277f1894c5b71839862a8989948b7b4
jorgee02 <jjacque@uc.cl>
2024-11-04 15:00:31 -0300

    primeros avances navbar

---
+++ front-SmilingFriends/src/common/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+export default function Navbar() {
+  return (
+
+    <nav className="navbar"> 
+      <h1>React Router</h1>    
+    </nav>
+    
+
+
+    
+  );
+};
