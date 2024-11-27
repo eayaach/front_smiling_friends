99616a598c183ecd1a8e98c0deb41da4245b20f7
jorgee02 <jjacque@uc.cl>
2024-11-27 02:31:31 -0300

    bloqueo/desbloqueo + avances en botar

--- front-SmilingFriends/src/game/InGame.jsx
+++ front-SmilingFriends/src/game/InGame.jsx
@@ -13,10 +13,10 @@ import MessageBox from './MessageBox';
 
 const Round = styled.div`
   background: linear-gradient(145deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.8));
-  border: 2px solid rgba(0, 255, 255, 0.8); /* Crea un borde brillante */
-  border-radius: 15px; /* Esquinas redondeadas */
+  border: 2px solid rgba(0, 255, 255, 0.8); 
+  border-radius: 15px; 
   box-shadow: 0 0 15px rgba(0, 255, 255, 0.5),
-              0 0 30px rgba(0, 255, 255, 0.3); /* Sombra con brillo */
+              0 0 30px rgba(0, 255, 255, 0.3); 
   display: flex;
   padding: 10px;
   align-items: center;
@@ -24,8 +24,8 @@ const Round = styled.div`
   color: white;
   font-size: 0.8em;
   text-shadow: 0 0 5px rgba(0, 255, 255, 0.8),
-               0 0 10px rgba(0, 255, 255, 0.6); /* Brillo en el texto */
-  animation: glow 1.5s infinite alternate; /* Animación para brillo */
+               0 0 10px rgba(0, 255, 255, 0.6); 
+  animation: glow 1.5s infinite alternate;
   @keyframes glow {
   from {
     box-shadow: 0 0 15px rgba(0, 255, 255, 0.5),
@@ -38,50 +38,39 @@ const Round = styled.div`
   }
 `;
 
-
 function InGame() {
   const [gameInfo, setGameInfo] = useState({ jugadores: [], cartas: [], tablero: null });
   const [error, setError] = useState(false);
   const [cartas, setCartas] = useState([]);
   const [skins, setSkins] = useState([]);
-  const [isLoading, setIsLoading] = useState(true); // Estado para manejar la carga
-  const [selectedCard, setSelectedCard] = useState(null); // Carta seleccionada
+  const [isLoading, setIsLoading] = useState(true);
+  const [selectedCard, setSelectedCard] = useState(null);
   const { token } = useContext(AuthContext);
   const { socket } = useContext(SocketContext);
   const userId = sessionStorage.getItem("user_id");
   const [msg_jugada, SetMsgJugada] = useState([]);
 
-  // recordar que debe ser:
-   // json jugada
-  /*  {id_saboteur: int,
-      jugador: int,
-      carta: integer,
-      accion: bool,
-      casilla : [posx, posy],
-      objetivo: int} */
-  const Comunication_data = {id_saboteur: null,
+  const Comunication_data = {
+    id_saboteur: null,
     jugador: null,
     carta: null,
     accion: null,
-    casilla : null,
+    casilla: null,
     objetivo: null,
     id_jugador: userId
-  }
+  };
 
   useEffect(() => {
     async function fetchData() {
       try {
-        // Cargar cartas e imágenes
         const cartasData = await cargar_cartas();
         const cartas = Object.keys(cartasData).map(clave => cartasData[clave]);
         setCartas(cartas);
 
-        // Cargar skins
         const skinsData = await cargar_skins();
         const skins = Object.keys(skinsData).map(clave => skinsData[clave]);
         setSkins(skins);
 
-        // Obtener datos de la partida desde el sessionStorage
         const partida = JSON.parse(sessionStorage.getItem("Partida"));
         setGameInfo(partida);
       } catch (error) {
@@ -93,17 +82,17 @@ function InGame() {
     }
     fetchData();
   }, []);
-  // este  Effect es para cuando recibe info nuevo
+
   useEffect(() => {
     if (!socket?.current) return;
 
     const handleUpdateGame = (response) => {
-      setIsLoading(true)
-      console.log(response)
-      setGameInfo(response)
-      SetMsgJugada(response.msg)
+      setIsLoading(true);
+      console.log(response);
+      setGameInfo(response);
+      SetMsgJugada(response.msg);
       sessionStorage.setItem('Partida', JSON.stringify(response));
-      setIsLoading(false)
+      setIsLoading(false);
     };
 
     socket.current?.on('InGamePartida', handleUpdateGame);
@@ -113,78 +102,151 @@ function InGame() {
     };
   }, [socket.current]);
 
+  const handlePlayerSelect = async (jugadorId) => {
+    if (!selectedCard || selectedCard[0].tipo === undefined) {
+      console.log("Selecciona una carta válida antes de jugar.");
+      return;
+    }
+  
+    // Asignar los valores correctamente a Comunication_data
+    Comunication_data.id_saboteur = parseInt(gameInfo.id_saboteur, 10); // Debería ser un número
+    Comunication_data.jugador = parseInt(userId, 10); // El id del jugador actual
+    Comunication_data.carta = parseInt(selectedCard[1], 10); // La carta seleccionada
+    Comunication_data.objetivo = jugadorId; // El objetivo es el jugador seleccionado
+    Comunication_data.accion = 1; // Acción de bloquear/desbloquear (o la acción correspondiente)
+    Comunication_data.casilla = "vacio"; // Asegúrate de que `casilla` esté correctamente asignada si es necesario
+  
+    console.log("Datos enviados al backend:", Comunication_data);
+    if (parseInt(gameInfo.turno_actual, 10) === parseInt(userId, 10)) {
+      try {
+        const response = await axios.post(
+          `${import.meta.env.VITE_BACKEND_URL}/partidas/play`,
+          Comunication_data,
+          { headers: { Authorization: `Bearer ${token}` } }
+        );
+        console.log("Respuesta del backend:", response.data);
+      
+        // Verificar si la respuesta contiene un mensaje de éxito
+        if (response.data.msg[0] === true) {
+          setIsLoading(true);
+          setGameInfo(response.data);
+          SetMsgJugada(response.data.msg);
+          sessionStorage.setItem('Partida', JSON.stringify(response.data));
+          setIsLoading(false);
+          setSelectedCard(null);
+        }
+      } catch (err) {
+        // Mostrar detalles del error
+        console.error("Error al jugar carta:", err.response?.data || 'Error desconocido');
+        setError(err.response?.data?.msg || 'Error al intentar jugar carta');
+      }
+    }    
+  };
+  
+  const handleDiscardCard = async () => {
+    if (!selectedCard) {
+      console.log("No hay carta seleccionada para descartar.");
+      return;
+    }
 
-  // Manejador de clics en las celdas del tablero
-  // jugada de poner carta en tablero
+    // Datos para descartar la carta
+    Comunication_data.id_saboteur = parseInt(gameInfo.id_saboteur, 10);
+    Comunication_data.jugador = parseInt(userId, 10);
+    Comunication_data.carta = parseInt(selectedCard[1], 10);
+    Comunication_data.accion = 0; // Acción de descartar
+    Comunication_data.objetivo = -1; // Sin objetivo
+    Comunication_data.casilla = "vacio"; // No hay casilla
+
+    console.log("Descartar carta:", Comunication_data);
+
+    if (parseInt(gameInfo.turno_actual, 10) === parseInt(userId, 10)) {
+      try {
+        const response = await axios.post(
+          `${import.meta.env.VITE_BACKEND_URL}/partidas/play`,
+          Comunication_data,
+          { headers: { Authorization: `Bearer ${token}` } }
+        );
+
+        console.log("Respuesta del backend:", response.data);
+
+        if (response.data.msg[0] === true) {
+          setIsLoading(true);
+          setGameInfo(response.data);
+          SetMsgJugada(response.data.msg);
+          sessionStorage.setItem('Partida', JSON.stringify(response.data));
+          setIsLoading(false);
+          setSelectedCard(null); // Limpiar carta seleccionada después de descartarla
+        }
+      } catch (err) {
+        console.error("Error al intentar descartar carta:", err.response?.data || 'Error desconocido');
+        setError(err.response?.data?.msg || 'Error al intentar descartar carta');
+      }
+    }
+  };
+
   const handleCellClick = async (filaIndex, celdaIndex) => {
     if (!selectedCard) {
       console.log("No hay carta seleccionada.");
       return;
     }
+    console.log("Datos enviados al backend:", Comunication_data);
+    if (parseInt(gameInfo.turno_actual, 10) === parseInt(userId, 10)) {
+      console.log("carta de indice:", selectedCard[1]);
 
-    if (parseInt(gameInfo.turno_actual,10) === parseInt(userId,10)){
-      // turno del que esta en pantalla
-      // mandamos la solicitud a ver si es valida la jugada
-      console.log("carta de indice:", selectedCard[1])
-      // ahora que se tiene la carta seleccionada
-      // tenemos dos caso jugar una carta de utilidad y una carta de camino
-      // este handle es solo para cartas de camnio
-      if (selectedCard[0].tipo === undefined){
+      if (selectedCard[0].tipo === undefined) {
         try {
-          Comunication_data.id_saboteur = parseInt(gameInfo.id_saboteur, 10)
-          Comunication_data.jugador = parseInt(userId, 10)
-          Comunication_data.carta = parseInt(selectedCard[1],10)
-          Comunication_data.casilla = [filaIndex, celdaIndex] // [pos_x, pos_y]
-          Comunication_data.objetivo = -1
-          Comunication_data.accion = 1
+          Comunication_data.id_saboteur = parseInt(gameInfo.id_saboteur, 10);
+          Comunication_data.jugador = parseInt(userId, 10);
+          Comunication_data.carta = parseInt(selectedCard[1], 10);
+          Comunication_data.casilla = [filaIndex, celdaIndex]; // [pos_x, pos_y]
+          Comunication_data.objetivo = -1;
+          Comunication_data.accion = 1;
 
           const response = await axios.post(
-            `${import.meta.env.VITE_BACKEND_URL}/partidas/play`, Comunication_data,
-            {
-              headers: {
-                Authorization: `Bearer ${token}`,
-              },
-            }
+            `${import.meta.env.VITE_BACKEND_URL}/partidas/play`,
+            Comunication_data,
+            { headers: { Authorization: `Bearer ${token}` } }
           );
-          console.log(response.data);
-          if (response.data.msg[0] === true || response.data.msg[0] === -1 || response.data.msg[0] === -2){
-            // jugada valida tenemos que renderizar el tablero y las cartas
-            setIsLoading(true)
-            setGameInfo(response.data)
-            SetMsgJugada(response.data.msg)
+
+          console.log("Respuesta del backend:", response.data);
+
+          if (response.data.msg[0] === true || response.data.msg[0] === -1 || response.data.msg[0] === -2) {
+            setIsLoading(true);
+            setGameInfo(response.data);
+            SetMsgJugada(response.data.msg);
             sessionStorage.setItem('Partida', JSON.stringify(response.data));
-            setIsLoading(false)
+            setIsLoading(false);
           }
         } catch (err) {
           console.log(err);
-          setError(err.response?.data?.msg || 'Error al intentar iniciar partida');
+          setError(err.response?.data?.msg || 'Error al intentar jugar la carta');
         }
       }
     }
-    // Desseleccionar la carta después de colocarla
     setSelectedCard(null);
   };
 
   return (
     <div className="section">
       {isLoading ? (
-        <div>Cargando datos...</div> // Puedes usar un componente de carga más visual
+        <div>Cargando datos...</div> 
       ) : (
         <>
           <Round>Turno Actual: {gameInfo.jugadores.find(jugador => jugador.id === gameInfo.turno_actual).usuario}</Round>
           <TableroWidget
             tablero={gameInfo.tablero}
             imagenes={cartas}
-            onCellClick={handleCellClick} // Pasa el manejador al tablero
+            onCellClick={handleCellClick} 
           />
-          <MessageBox msg = {msg_jugada} mazo={gameInfo.cartas_restantes}></MessageBox>
+          <MessageBox msg={msg_jugada} mazo={gameInfo.cartas_restantes}></MessageBox>
           <PlayerCards
             value={selectedCard}
             cartas={gameInfo.cartas}
             imagenes={cartas}
-            setter={setSelectedCard} // Establecer la carta seleccionada
+            setter={setSelectedCard} 
+            OnDiscard={handleDiscardCard}
           />
-          <Players jugadores={gameInfo.jugadores} imagenes={skins} userId={userId} />
+          <Players jugadores={gameInfo.jugadores} imagenes={skins} userId={userId} onPlayerSelect={handlePlayerSelect} />
           <Me jugadores={gameInfo.jugadores} imagenes={skins} userId={userId} is_evil={gameInfo.is_evil} />
         </>
       )}
--- front-SmilingFriends/src/gamecomponents/PlayerCards.jsx
+++ front-SmilingFriends/src/gamecomponents/PlayerCards.jsx
@@ -23,31 +23,86 @@ const ContenedorCartas = styled.div`
     }
 `;
 
-function PlayerCards({ cartas = [], imagenes = {}, value, setter }) {
+const DiscardButton = styled.button`
+  position: fixed;
+  bottom: 200px;
+  right: 180px;
+  background: linear-gradient(145deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.8));
+  border: 2px solid rgba(255, 0, 0, 0.8); 
+  border-radius: 15px; 
+  box-shadow: 0 0 15px rgba(255, 0, 0, 0.5),
+              0 0 30px rgba(255, 0, 0, 0.3); 
+  color: white;
+  padding: 10px 20px;
+  font-size: 1rem;
+  font-weight: bold;
+  max-width: 200px;
+  text-align: center;
+  text-shadow: 0 0 5px rgba(255, 0, 0, 0.8),
+               0 0 10px rgba(255, 0, 0, 0.6);
+  cursor: pointer;
+  transition: background-color 0.3s, box-shadow 0.3s;
+
+  /* Animación exclusiva para el botón */
+  animation: buttonGlow 1.5s infinite alternate;
+
+  @keyframes buttonGlow {
+    from {
+      box-shadow: 0 0 15px rgba(255, 0, 0, 0.5),
+                  0 0 30px rgba(255, 0, 0, 0.3);
+    }
+    to {
+      box-shadow: 0 0 25px rgba(255, 0, 0, 0.8),
+                  0 0 50px rgba(255, 0, 0, 0.5);
+    }
+  }
+
+  :hover {
+    background-color: rgba(255, 0, 0, 0.8);
+    box-shadow: 0 0 25px rgba(255, 0, 0, 0.8),
+                0 0 50px rgba(255, 0, 0, 0.6);
+  }
+
+  :disabled {
+    background: linear-gradient(145deg, rgba(128, 128, 128, 0.5), rgba(64, 64, 64, 0.8));
+    border: 2px solid rgba(192, 192, 192, 0.8);
+    box-shadow: 0 0 10px rgba(128, 128, 128, 0.5),
+                0 0 20px rgba(128, 128, 128, 0.3);
+    cursor: not-allowed;
+    text-shadow: 0 0 5px rgba(128, 128, 128, 0.8),
+                 0 0 10px rgba(128, 128, 128, 0.6);
+  }
+`;
+
+
+function PlayerCards({ cartas = [], imagenes = {}, value, setter, OnDiscard }) {
   // Handle the card selection and update the state in the parent component
   const handleChange = (indice) => {
-    const cartaSeleccionada = cartas[indice];  // Get the selected card data
+    const cartaSeleccionada = cartas[indice]; // Get the selected card data
     console.log(`Carta seleccionada: índice ${indice}, datos:`, cartaSeleccionada);
-    setter([cartaSeleccionada, indice]);  // ajustamos esto para que guarde indice tambien par apoder hacer las consultas
+    setter([cartaSeleccionada, indice]); // Adjust to store index for queries
   };
 
-
-
   return (
-    <ContenedorCartas>
-      {cartas.map((carta, cartaIndice) => {
-        const imageSrc = imagenes[carta.id_imagen]; // Get the image source for the card
+    <>
+      <ContenedorCartas>
+        {cartas.map((carta, cartaIndice) => {
+          const imageSrc = imagenes[carta.id_imagen]; // Get the image source for the card
 
-        return (
-          <Card
-            key={cartaIndice}
-            indice={cartaIndice}
-            imagen={imageSrc}
-            onClick={() => handleChange(cartaIndice)} // Call handleChange on click to update parent state
-          />
-        );
-      })}
-    </ContenedorCartas>
+          return (
+            <Card
+              key={cartaIndice}
+              indice={cartaIndice}
+              imagen={imageSrc}
+              onClick={() => handleChange(cartaIndice)} // Call handleChange on click to update parent state
+            />
+          );
+        })}
+      </ContenedorCartas>
+      <DiscardButton onClick={OnDiscard} disabled={!value}>
+        Descartar
+      </DiscardButton>
+    </>
   );
 }
 
